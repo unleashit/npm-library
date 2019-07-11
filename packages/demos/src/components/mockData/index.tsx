d86222d8deeab3274b148dff9d69336ac5b0449c
unleashit <unleashit@protonmail.com>
2019-07-11 15:05:24 -0700

    feat(demos): add mock-data demo

---
+++ packages/demos/src/components/mockData/index.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import AsyncHandler from '@unleashit/async-handler';
+import RecursiveDataLister from '@unleashit/recursive-data-lister';
+import { AppContext } from '../../utils/context';
+import './mock-data.scss';
+
+export class PaginationDemo extends React.Component<{}> {
+  static contextType = AppContext;
+
+  request1 = () => this.context.store.generateFakeUsers({ total: 3 });
+
+  request2 = () => this.context.store.generateFakeProducts({ total: 3 });
+
+  request3 = () => this.context.store.generateComplexJson({ total: 3 });
+
+  render() {
+    return (
+      <div>
+        <h2>List of Users (user template)</h2>
+        <AsyncHandler request={this.request1}>
+          {(data: any[]) => <RecursiveDataLister data={data} />}
+        </AsyncHandler>
+        <h2>List of Products (product template)</h2>
+        <AsyncHandler request={this.request2}>
+          {(data: any[]) => <RecursiveDataLister data={data} />}
+        </AsyncHandler>
+        <h2>Nested Data (custom template)</h2>
+        <AsyncHandler request={this.request3}>
+          {(data: any[]) => <RecursiveDataLister data={data} />}
+        </AsyncHandler>
+      </div>
+    );
+  }
+}
+
+export default PaginationDemo;
