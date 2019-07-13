28c1a2707062a8b25f10cab8057528d3e81851ab
unleashit <unleashit@protonmail.com>
2019-07-12 17:06:53 -0700

    build(root): testing ci/cd

--- jest.config.js
+++ jest.config.js
@@ -34,5 +34,10 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/../../__mocks__/fileMock.js",
     "\\.(css|scss)$": "identity-obj-proxy",
     "\\.svg$": "@svgr/webpack"
+  },
+  globals: {
+    'ts-jest': {
+      diagnostics: false
+    }
   }
 };
