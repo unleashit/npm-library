72153687ea39f3258f5c629e9dba58f47f2caffa
unleashit <unleashit@protonmail.com>
2020-05-20 23:09:09 -0700

    build: allow missing return types in eslint for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,27 @@
-module.exports =  {
-  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
-  plugins: ["@typescript-eslint", "import", "react-hooks"],
-  extends:  [
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  plugins: ['@typescript-eslint', 'import', 'react-hooks'],
+  extends: [
     'airbnb',
-    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier',
-    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     // 'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
-  parserOptions:  {
-    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
-    sourceType:  'module',  // Allows for the use of imports
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
     // project: "./tsconfig.json"
   },
   rules: {
     '@typescript-eslint/no-var-requires': 0, // to be compatible with JS/Node config files
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/explicit-member-accessibility': 0,
-    '@typescript-eslint/no-unused-vars': [2, { 'varsIgnorePattern': '^_' , 'argsIgnorePattern': '^_'}],
+    '@typescript-eslint/no-unused-vars': [
+      2,
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 0,
     // 'no-unused-vars': [2, { "argsIgnorePattern": "^_" }],
     'no-console': process.NODE_ENV === 'production' ? 2 : 0,
     'react/jsx-filename-extension': 0,
@@ -26,30 +30,33 @@ module.exports =  {
     'no-underscore-dangle': 0,
     'import/prefer-default-export': 0,
     'react/prop-types': 0,
-    "jsx-a11y/label-has-for": [ 2, {
-      "components": [ "Label" ],
-      "required": {
-        "some": [ "nesting", "id" ]
+    'jsx-a11y/label-has-for': [
+      2,
+      {
+        components: ['Label'],
+        required: {
+          some: ['nesting', 'id'],
+        },
+        allowChildren: false,
       },
-      "allowChildren": false
-    }],
-    "class-methods-use-this": 0,
-    "no-alert": 0,
-    "react/prefer-stateless-function": 1,
-    "react/destructuring-assignment": 0,
-    "no-plusplus": 0,
-    "react/sort-comp": 0, // temp
-    "no-return-await": 0, // temp
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    ],
+    'class-methods-use-this': 0,
+    'no-alert': 0,
+    'react/prefer-stateless-function': 1,
+    'react/destructuring-assignment': 0,
+    'no-plusplus': 0,
+    'react/sort-comp': 0, // temp
+    'no-return-await': 0, // temp
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
-  "env": {
-    "browser": true,
-    "jest": true
+  env: {
+    browser: true,
+    jest: true,
   },
-  "globals": {
-    "shallow": true,
-    "render": true,
-    "mount": true
-  }
+  globals: {
+    shallow: true,
+    render: true,
+    mount: true,
+  },
 };
