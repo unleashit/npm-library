add7b5bfe9815183ef2551f61f5ae72c9da979b9
unleashit <unleashit@protonmail.com>
2023-05-23 17:33:48 -0700

    build(all): improve treeshaking of builds

--- shared/configs/tsup/tsup.config.ts
+++ shared/configs/tsup/tsup.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'tsup';
 const env = process.env.NODE_ENV;
 
 export const tsup = defineConfig({
-  splitting: true,
   clean: env === 'production', // clean up the dist folder
+  splitting: true,
   dts: true, // generate dts files
   format: ['cjs', 'esm'], // generate cjs and esm files
+  treeshake: true,
   minify: env === 'production',
   bundle: true,
   skipNodeModulesBundle: true,
